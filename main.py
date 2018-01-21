96c2823f7fe40eedbdddbb97475a093b838aec1f
KLJ42 <klj42@mailbox.org>
2018-01-21 19:24:17 +0100

    first window
    Signed-off-by: KLJ42 <klj42@mailbox.org>

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from tkinter import *
 
-root =
+# create empty window
+root = Tk()
+
+# create label
+theLabel = Label(root,text="This is my text")
+theLabel.pack()
+
+# keep window open
+root.mainloop()
