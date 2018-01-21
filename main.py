84a9b9fb453b13ab2a9f9b2452d68ac6d63b2e52
KLJ42 <klj42@mailbox.org>
2018-01-21 19:35:03 +0100

    getting buttons
    Signed-off-by: KLJ42 <klj42@mailbox.org>

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from tkinter import *
 # create empty window
 root = Tk()
 
-# create label
-theLabel = Label(root,text="This is my text")
-theLabel.pack()
+# window management
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+#widget
+button_input_1 = Button(topFrame, text="Get PDF 1", fg="blue")
+button_input_2 = Button(topFrame, text="Get PDF 2", fg="red")
+button_output = Button(topFrame, text="Output")
+button_merge = Button(bottomFrame, text="merge", fg="green")
+
+# display button
+button_input_1.pack(side=LEFT)
+button_input_2.pack(side=LEFT)
+button_output.pack(side=LEFT)
+button_merge.pack(side=BOTTOM)
 
 # keep window open
 root.mainloop()
